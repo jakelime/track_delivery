8154dbacf99756ce44ea237321c499f1d0bad8e6
Jake Lim <lim.jiahao@stengg.com>
2024-03-07 17:34:54 +0800

    it works!

--- tdds/dhl.py
+++ tdds/dhl.py
@@ -2,13 +2,22 @@ import http.client
 import json
 import os
 import urllib.parse
-
+import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-params = urllib.parse.urlencode({"trackingNumber": "7777777770", "service": "express"})
+def get_current_dt(dtfmtstr: str = "%Y%m%d_%H%M%S", append_us: bool = True):
+    now = datetime.now()
+    timestr = now.strftime()
+    if append_us:
+        timestr_ms = now.strftime("%f")[:2]
+        timestr = f"{timestr}{timestr_ms}"
+    return timestr
+
+
+params = urllib.parse.urlencode({"trackingNumber": "2819479703", "service": "express"})
 
 headers = {"Accept": "application/json", "DHL-API-Key": os.environ.get("DHL_API_KEY")}
 
@@ -22,6 +31,8 @@ reason = response.reason
 data = json.loads(response.read())
 
 print("Status: {} and reason: {}".format(status, reason))
-print(data)
-
+print(f"{len(data)=}")
+with open("results.json", "w") as out_file:
+    json.dump(data, out_file, indent=6)
+    
 connection.close()
